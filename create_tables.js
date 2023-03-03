af8681af42fb861fd84fdf002b5b201b52d195d0
Gabriel Fairbairn <97549201+NerdSlayer0@users.noreply.github.com>
2023-03-03 15:50:36 -0800

    createtables.js launches query to create usertype

---
+++ create_tables.js
@@ -0,0 +1,48 @@
+const database = include('databaseConnection');
+
+// async function createTables() {
+//     let createUserSQL = `
+//         CREATE TABLE IF NOT EXISTS users (
+//             user_id INT NOT NULL AUTO_INCREMENT,
+//             username VARCHAR(25) NOT NULL,
+//             password VARCHAR(100) NOT NULL,
+//             email VARCHAR(100) NOT NULL,
+//             PRIMARY KEY (user_id),
+//             UNIQUE INDEX username_unique (username ASC)
+//     );`;
+
+//     try {
+//         const results = await database.query(createUserSQL);
+//         console.log("Successfully created tables");
+//         console.log(results[0]);
+//         return true;
+//     } catch(err) {
+//         console.log("error creating tables");
+//         console.log(err);
+//         return false;
+//     }
+// }
+
+async function createTables() {
+    let createUserSQL = `
+        CREATE TABLE user_type (
+            user_type_id INT NOT NULL AUTO_INCREMENT,
+            type VARCHAR(25) NOT NULL,
+            PRIMARY KEY(user_type_id),
+            UNIQUE KEY 'unique_type'('type')
+    );`;
+
+    try {
+        const results = await database.query(createUserSQL);
+        console.log("Successfully created tables");
+        console.log(results[0]);
+        return true;
+    } catch(err) {
+        console.log("error creating tables");
+        console.log(err);
+        return false;
+    }
+}
+
+module.exports = {createTables};
+
